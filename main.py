e96a69859af5ce95c62cffe1180b26058e1982bd
Andriel Alexandre <an-driel@hotmail.com>
2022-05-06 21:24:43 -0300

    Resolução de exercícios

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-nome = "Philipe"
-sobrenome = "Oliveira"
+# nome = "Philipe"
+# sobrenome = "Oliveira"
+#
+# print("Meu nome é", nome, "e esse é meu primeiro print de tela")
 
-print("Meu nome é", nome, sobrenome)
+# Atividade 5 - Calcule e apresente o produto entre 10 * 50
+
+# num_01 = 10
+# num_02 = 50
+# produto = num_01 * num_02
+#
+# print("O produto de", num_01, "*", num_02, "é:", produto)
+#
+# # Atividade 6 - Calcule a média entre 8, 9 e 7
+#
+# media = (8 + 9 + 7) / 3
+#
+# print(f"A média é: {media}. Esta variável é do tipo {type(media).__name__}")
+
+# Atividade 8 - Peça ao usuário para seu nome e sobrenome e apresente em tela
+
+# user_name = input("Digite seu nome: ")
+# user_surname = input("Digite seu sobrenome: ")
+# complete_name = f"{user_name} {user_surname}"
+# print(f"Seu nome completo é: {complete_name}")
+
+# Atividade 9 - Peça ao usuário dois números inteiros e apresente a soma dos dois
+
+int_num_01 = int(input("Número 01: "))
+int_num_02 = int(input("Número 02: "))
+
+result = int_num_01 + int_num_02
+
+print(f"A soma de {int_num_01} + {int_num_02} = {result}")
