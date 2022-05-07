62e54cc6d61dd445e2fa4dea4466013b995492bd
philmcqueid <philmcqueid@gmail.com>
2022-05-07 13:54:01 -0300

    Atividades da aula 1 adicionadas

---
+++ atividades_aula_01_portal_do_aluno.py
@@ -0,0 +1,26 @@
+# # Atividade 01 — Calcular o IMC:
+#
+# weight = float(input("Digite seu peso: "))
+# height = float(input("Digite sua altura: "))
+#
+# imc = weight / (height ** 2)
+#
+# print(f"Seu imc é: {round(imc, 2)}")
+
+# # Atividade 02 — Transformar uma temperatura de Fahrenheit para Celsius:
+# print("Conversor de temperatura Celsius para Fahrenheit")
+# temperature_in_celsius = int(input("Qual tempera deseja converter: "))
+#
+# temperature_in_fahrenheit = (temperature_in_celsius * 9 / 5) + 32
+#
+# print(f"{temperature_in_celsius}° celsius é igual a {temperature_in_fahrenheit}° Fahrenheit")
+
+# # Atividade 03 — Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a
+# # área do retângulo (base*altura).
+# print("Calcular a área interna de um retângulo")
+# base = int(input("Qual o valor da base do retângulo: "))
+# height = int(input("Qual o valor da altura do retângulo: "))
+#
+# area = base * height
+# print(f"A área toral do retângulo é de {area} m²")
+
--- main.py
+++ main.py
@@ -1,67 +1,67 @@
-# nome = "Philipe"
-# sobrenome = "Oliveira"
-#
-# print("Meu nome é", nome, "e esse é meu primeiro print de tela")
+nome = "Philipe"
+sobrenome = "Oliveira"
+
+print("Meu nome é", nome, "e esse é meu primeiro print de tela")
 
 # Atividade 5 - Calcule e apresente o produto entre 10 * 50
 
-# num_01 = 10
-# num_02 = 50
-# produto = num_01 * num_02
-#
-# print("O produto de", num_01, "*", num_02, "é:", produto)
-#
-# # Atividade 6 - Calcule a média entre 8, 9 e 7
-#
-# media = (8 + 9 + 7) / 3
-#
-# print(f"A média é: {media}. Esta variável é do tipo {type(media).__name__}")
+num_01 = 10
+num_02 = 50
+produto = num_01 * num_02
+
+print("O produto de", num_01, "*", num_02, "é:", produto)
+
+# Atividade 6 - Calcule a média entre 8, 9 e 7
+
+media = (8 + 9 + 7) / 3
+
+print(f"A média é: {media}. Esta variável é do tipo {type(media).__name__}")
 
 # Atividade 8 - Peça ao usuário para seu nome e sobrenome e apresente em tela
 
-# user_name = input("Digite seu nome: ")
-# user_surname = input("Digite seu sobrenome: ")
-# complete_name = f"{user_name} {user_surname}"
-# print(f"Seu nome completo é: {complete_name}")
+user_name = input("Digite seu nome: ")
+user_surname = input("Digite seu sobrenome: ")
+complete_name = f"{user_name} {user_surname}"
+print(f"Seu nome completo é: {complete_name}")
 
 # Atividade 9 - Peça ao usuário dois números inteiros e apresente a soma dos dois
 
-# int_num_01 = int(input("Número 01: "))
-# int_num_02 = int(input("Número 02: "))
-#
-# result = int_num_01 + int_num_02
-#
-# print(f"A soma de {int_num_01} + {int_num_02} = {result}")
+int_num_01 = int(input("Número 01: "))
+int_num_02 = int(input("Número 02: "))
+
+result = int_num_01 + int_num_02
+
+print(f"A soma de {int_num_01} + {int_num_02} = {result}")
 
 # Atividade 10 - Refaça o programa das médias, agora com 4 notas, pedindo as notas para o usuário, e, por fim
 # mostrar o resultado em tela
 
-# nota_01 = float(input("Digite a nota 01: "))
-# nota_02 = float(input("Digite a nota 02: "))
-# nota_03 = float(input("Digite a nota 03: "))
-# nota_04 = float(input("Digite a nota 04: "))
-# print("\n")
-#
-# media = (nota_01 + nota_02 + nota_03 + nota_04) / 4
-#
-# print(f"Sua média final foi: {media}")
+nota_01 = float(input("Digite a nota 01: "))
+nota_02 = float(input("Digite a nota 02: "))
+nota_03 = float(input("Digite a nota 03: "))
+nota_04 = float(input("Digite a nota 04: "))
+print("\n")
+
+media = (nota_01 + nota_02 + nota_03 + nota_04) / 4
+
+print(f"Sua média final foi: {media}")
 
 # Atividade 11 - Peça dois números ao usuário, mostre os resultados de uma calculadora básica
 
-# print("\n")  # Apenas para dar espaçamento
-# number_01 = int(input("Digite o primeiro número: "))
-# number_02 = int(input("Digite o segundo número: "))
-# print("\n")  # Apenas para dar espaçamento
-#
-# adicao = number_01 + number_02
-# subtracao = number_01 - number_02
-# multiplicacao = number_01 * number_02
-# divisao = number_01 / number_02
-#
-# print(f"O resultado de {number_01} + {number_02} = {adicao}")
-# print(f"O resultado de {number_01} - {number_02} = {subtracao}")
-# print(f"O resultado de {number_01} * {number_02} = {multiplicacao}")
-# print(f"O resultado de {number_01} / {number_02} = {divisao}")
+print("\n")  # Apenas para dar espaçamento
+number_01 = int(input("Digite o primeiro número: "))
+number_02 = int(input("Digite o segundo número: "))
+print("\n")  # Apenas para dar espaçamento
+
+adicao = number_01 + number_02
+subtracao = number_01 - number_02
+multiplicacao = number_01 * number_02
+divisao = number_01 / number_02
+
+print(f"O resultado de {number_01} + {number_02} = {adicao}")
+print(f"O resultado de {number_01} - {number_02} = {subtracao}")
+print(f"O resultado de {number_01} * {number_02} = {multiplicacao}")
+print(f"O resultado de {number_01} / {number_02} = {divisao}")
 
 
 print("677", "031", "123", sep=".", end="-")
