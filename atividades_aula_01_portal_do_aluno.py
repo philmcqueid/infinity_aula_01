d83f494d05e20b4b98e98b6a459f97fe30bf5d80
philmcqueid <philmcqueid@gmail.com>
2022-05-07 14:57:10 -0300

    Exercícios finalizados até a Atividade 04

--- atividades_aula_01_portal_do_aluno.py
+++ atividades_aula_01_portal_do_aluno.py
@@ -24,3 +24,25 @@
 # area = base * height
 # print(f"A área toral do retângulo é de {area} m²")
 
+# # Atividade 04 — Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos em
+# # branco, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores.
+
+
+number_of_voters = int(input("Qual a quantidade de eleitores: "))
+valid_votes = int(input("Qual a quantidade de votos válidos: "))
+blank_votes = int(input("Qual a quantidade de votos em branco: "))
+null_votes = int(input("Qual a quantidade de votos nulos: "))
+
+total_votes = valid_votes + blank_votes + null_votes
+
+percent_valid_votes = round(valid_votes * 100 / number_of_voters, 2)
+percent_blank_votes = round(blank_votes * 100 / number_of_voters, 2)
+percent_null_votes = round(null_votes * 100 / number_of_voters, 2)
+
+no_voters = number_of_voters - total_votes
+
+print(f"\nTotal de eleitores: {number_of_voters}")
+print(f"{percent_valid_votes}% dos votos foram válidos.")
+print(f"{percent_blank_votes}% dos eleitores votaram em branco")
+print(f"{percent_null_votes}% dos eleitores votaram nulo\n")
+print(f"Ao todos {no_voters} não votaram. Representam {round(no_voters * 100 / number_of_voters, 2)}% da população")
