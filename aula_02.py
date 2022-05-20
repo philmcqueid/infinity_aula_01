dec5f9e19cda92f62001ba112f783ee39afc411c
Philipe <philmcqueid@gmail.com>
2022-05-20 20:18:32 -0300

    Aula 2 Adicionada

---
+++ aula_02.py
@@ -0,0 +1,29 @@
+# """01"""
+# user_chosen_number = int(input("Escolha um número: "))
+# print(pow(user_chosen_number, 3))
+# """02"""
+# number = 1.500
+# print(round(number))
+# """"imports"""
+# import math
+#
+# print(math.ceil(15.6))
+
+# negative_value = int(input("Escolha um número inteiro: "))
+# print(abs(negative_value))
+
+# number_01 = int(input("Escolha um número: "))
+# number_02 = int(input("Escolha outro númeor: "))
+#
+# soma = number_01 + number_02
+#
+# print(f"A soma de {number_01} + {number_02} é {soma}")
+
+
+# number_with_five = 5.12458
+# print(f"{number_with_five:.2F}")
+
+print(5 > 10)  # False
+print(5 < 10)  # True
+print(5 < 10 and 8 < 3)  # False
+print(3 < 5 or 10 < 6)  # True
